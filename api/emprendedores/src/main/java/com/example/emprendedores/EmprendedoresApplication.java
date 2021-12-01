07dd629abc8c99c522572baca1dd914d4bdad3e0
Fredy Alejandro Moreno Castro <fmoreno@personalsoft.com.co>
2021-11-30 23:11:50 -0500

    commit api

---
+++ api/emprendedores/src/main/java/com/example/emprendedores/EmprendedoresApplication.java
@@ -0,0 +1,13 @@
+package com.example.emprendedores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmprendedoresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmprendedoresApplication.class, args);
+	}
+
+}
---
+++ api/emprendedores/src/test/java/com/example/emprendedores/EmprendedoresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.emprendedores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmprendedoresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
